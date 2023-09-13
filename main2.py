99e5dc76d58b46d5f81cf7f441fee83b3dd0b3fc
Mads Meinert Andersen <DMQ@kmd.dk>
2023-09-13 12:30:09 +0200

    sokoban robot functionality

--- main2.py
+++ main2.py
@@ -18,6 +18,8 @@ left_sensor = ColorSensor(Port.S1)
 right_sensor = ColorSensor(Port.S4)
 middle_sensor = ColorSensor(Port.S3)
 
+route = ["u", "l"]
+
 # Initialize drive base
 robot = DriveBase(left_motor, right_motor, wheel_diameter=442, axle_track=1600)
 
@@ -25,65 +27,92 @@ BLACK = 5
 GRAY = 80
 threshold = (BLACK + GRAY) / 2  # 42.5
 DRIVE_SPEED = 1000
+RIGHT_TURN = 90
+LEFT_TURN = -90
+TURN_180 = 180
+LEFT_DIRECTION = 'left'
+RIGHT_DIRECTION = 'right'
 
+RIGHT = 'r'
+LEFT = 'l'
+UP = 'u'
+DOWN = 'd'
 # Constants for correction behavior
 CORRECTION_DURATION = 500  # Time in milliseconds
 CORRECTION_SPEED = 300     # Speed for correction
 
-while True:
-    # Read sensor values
-    left_reflection = left_sensor.reflection()
-    right_reflection = right_sensor.reflection()
-    middle_reflection = middle_sensor.reflection()
-
-    # Determine the state based on sensor readings
-    if left_reflection < threshold and middle_reflection < threshold and right_reflection < threshold:
-        state = "front_and_left_and_right"
-    elif middle_reflection < threshold and left_reflection < threshold:
-        state = "front_and_left"
-    elif middle_reflection < threshold and right_reflection < threshold:
-        state = "front_and_right"
-    elif left_reflection < threshold and right_reflection < threshold:
-        state = "left_and_right"
-    elif left_reflection < threshold:
-        state = "left"
-    elif right_reflection < threshold:
-        state = "right"
-    elif middle_reflection < threshold:
-        state = "front"
-    else:
-        state = "none_black"
-
-    # Print sensor values and current state
-    print("Left:", left_reflection)
-    print("Middle:", middle_reflection)
-    print("Right:", right_reflection)
-    print("State:", state)
-
-    # Correction behavior when no sensors are on black
-    if state == "none_black":
+def correct_robot(direction):
+        if direction == LEFT_DIRECTION:
+            robot.turn(-20)
+        elif direction == RIGHT_DIRECTION:
+            robot.turn(20)
         robot.drive(CORRECTION_SPEED, 0)
         wait(CORRECTION_DURATION)
-        robot.stop()
 
-    # Make decisions based on the current state
+def drive_forward():
+    should_drive = True
+    robot.drive(DRIVE_SPEED, 0)
+    while should_drive:
+        left_reflection = left_sensor.reflection()
+        right_reflection = right_sensor.reflection()
+        middle_reflection = middle_sensor.reflection()
+
+        if left_reflection < threshold and middle_reflection < threshold and right_reflection < threshold:
+            should_drive = False
+        elif middle_reflection < threshold and left_reflection < threshold:
+            should_drive = False
+        elif middle_reflection < threshold and right_reflection < threshold:
+            should_drive = False
+        elif left_reflection < threshold:
+            correct_robot(LEFT_DIRECTION)
+        elif right_reflection < threshold:
+            correct_robot(RIGHT_DIRECTION)
+
+        wait(100)
+
+def turn_and_drive(turn):
+        robot.turn(turn)
+        drive_forward()
+  
+
+# def adjust_route(current_sign, previous_sign):
+
+
+for i, sign in route:
+    if i is not 0:
+        if sign == route[i - 1]:
+            drive_forward()
+        else:
+            if sign == UP and route[i - 1] == RIGHT:
+                turn_and_drive(LEFT_TURN)
+            elif sign == UP and route[i - 1] == LEFT:
+                turn_and_drive(RIGHT_TURN)
+            elif sign == UP and route[i - 1] == DOWN:
+                turn_and_drive(TURN_180)
+            elif sign == LEFT and route[i - 1] == UP:
+                turn_and_drive(LEFT_TURN)
+            elif sign == LEFT and route[i - 1] == DOWN:
+                turn_and_drive(RIGHT_TURN)
+            elif sign == LEFT and route[i - 1] == RIGHT:
+                turn_and_drive(TURN_180)
+            elif sign == DOWN and route[i - 1] == LEFT:
+                turn_and_drive(LEFT_TURN)
+            elif sign == DOWN and route[i - 1] == RIGHT:
+                turn_and_drive(RIGHT_TURN)
+            elif sign == DOWN and route[i - 1] == UP:
+                turn_and_drive(TURN_180)
+            elif sign == RIGHT and route[i - 1] == UP:
+                turn_and_drive(RIGHT_TURN)
+            elif sign == RIGHT and route[i - 1] == DOWN:
+                turn_and_drive(LEFT_TURN)
+            elif sign == RIGHT and route[i - 1] == LEFT:
+                turn_and_drive(TURN_180)
     else:
-        # Create a list of possible directions based on the current state
-        possible_directions = []
-        if "front" in state:
-            possible_directions.append("front")
-        if "left" in state:
-            possible_directions.append("left")
-        if "right" in state:
-            possible_directions.append("right")
-
-        random_decision = random.choice(possible_directions)
-
-        if random_decision == "front":
-            robot.drive(DRIVE_SPEED, 0)
-        elif random_decision == "left":
-            robot.turn(-90)
-        elif random_decision == "right":
-            robot.turn(90)
-
-    wait(100)
+        if sign == 'u':
+            drive_forward()
+        elif sign == 'r':
+            turn_and_drive(RIGHT_TURN)
+        elif sign == 'l':
+            turn_and_drive(LEFT_TURN)
+        elif sign == 'd':
+            turn_and_drive(TURN_180)
