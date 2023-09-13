e28888ee1d3d49aec66790b31475b2028b2b12f1
Mads Meinert Andersen <DMQ@kmd.dk>
2023-09-13 11:38:28 +0200

    clean up

--- bfs.py
+++ bfs.py
@@ -4,13 +4,10 @@ def print_results(board):
     print("Solution: " + board.getDirections())
 
 def search(board):
-    nodes_generated = 0
-    nodes_repeated = 0
     if board.is_win():
         print_results(board)
         return board
     initial_node = deepcopy(board)
-    nodes_generated += 1
     frontier = []
     frontier.append(initial_node)
     explored = set()
@@ -26,12 +23,9 @@ def search(board):
             explored.add(current_node)
             for move in moves:
                 child_node = deepcopy(current_node)
-                nodes_generated += 1
                 child_node.move(move)
                 if child_node not in explored:
                     if child_node.is_win():
                         print_results(child_node)
                         return child_node
                     frontier.append(child_node)
-                else:
-                    nodes_repeated += 1
