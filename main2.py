5962a876bd4d030f4b6a3aa7e3d9d2b37cff3e9e
frederikbf1 <frfa@itu.dk>
2023-09-20 09:37:00 +0200

    commits

--- main2.py
+++ main2.py
@@ -19,7 +19,9 @@ middle_sensor = ColorSensor(Port.S3)
 
 #route = ["u", "l", "u", "r", "d", "l", "d", "r"]
 #route = ["u", "l"]
-route = ["u", "r"]
+#route = ["up", "l"]#, "rp", "l"]
+#route = ["r", "r", "d", "l", "d", "l", "l", "u", "rp", "rp", "l", "l", "d", "r", "r", "r", "up", "up"]
+route = ["up", "up"]
 
 # Initialize drive base
 robot = DriveBase(left_motor, right_motor, wheel_diameter=826, axle_track=1700)
@@ -57,6 +59,8 @@ def drive_forward(speed):
     while should_drive:
         deviation = middle_sensor.reflection() - MIDDLE_SENSOR_THRESHOLD
         turn_rate = PROPORTIONAL_GAIN * deviation
+        if speed < 0:
+            turn_rate = turn_rate * -1
         robot.drive(speed, turn_rate)
 
         left_reflection = left_sensor.reflection()
@@ -87,8 +91,13 @@ def turn_and_drive(turn):
         drive_forward(DRIVE_SPEED)
 
 def push_diamond():
+    robot.straight(1000)
     drive_forward(DRIVE_SPEED)
-    drive_forward(-DRIVE_SPEED)
+    robot.straight(-1000)
+    turn_and_drive(180)
+    robot.straight(2000)
+    turn_and_drive(180)
+    #drive_forward(-DRIVE_SPEED)
 
 
 def drive_direction(sign, previousSign, containsP):
@@ -143,14 +152,14 @@ def drive_direction(sign, previousSign, containsP):
 
 for i, signs in enumerate(route):
     sign = signs[0]
-    previousSign = route[i - 1]
+    previousSign = route[i - 1][0]
     if i is not 0:
         if sign == route[i - 1][0]:
             drive_forward(DRIVE_SPEED)
             if "p" in signs:
                 push_diamond()
         elif "p" in signs:
-            drive_direction(sign, previousSign, True)
+            drive_direction(sign, previousSign, False)
             push_diamond()
         else:
             drive_direction(sign, previousSign, False)
@@ -163,3 +172,5 @@ for i, signs in enumerate(route):
             turn_and_drive(LEFT_TURN)
         elif sign == DOWN:
             turn_and_drive(TURN_180)
+        if "p" in signs:
+            push_diamond()
