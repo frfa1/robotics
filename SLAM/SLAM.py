dfc766997e216ec8a958d1da10ab82f8cd50d531
Mads Meinert Andersen <DMQ@kmd.dk>
2023-10-10 16:58:10 +0200

    try to update algorithms

--- SLAM/SLAM.py
+++ SLAM/SLAM.py
@@ -4,10 +4,8 @@ from breezyslam.sensors import RPLidarA1 as LaserModel
 from rplidar import RPLidar as Lidar
 from thymio_vehicle import ThymioVehicle
 from visualize_thymio_file import visualizer
-import multiprocessing
-from multiprocessing import Pipe
 import asyncio
-import json
+from multiprocessing import Pipe
 
 MAP_SIZE_PIXELS = 250
 MAP_SIZE_METERS = 15
@@ -15,20 +13,17 @@ LIDAR_DEVICE = '/dev/ttyUSB0'
 wheel_radius_mm = 21
 half_axl_length_mm = 45
 
-
 class LidarController:
-    def __init__(self, device, slam_obj, thymio_vehicle_obj):
+    def __init__(self, device, slam_obj, thymio_vehicle_obj, controller_pipe):
         self.lidar = Lidar(device)
         self.slam = slam_obj
         self.thymio = thymio_vehicle_obj
         self.viz = visualizer()
         self.pose = [0, 0, 0]
+        self.controller_pipe = controller_pipe
 
-        # Create an RMHC SLAM object with a laser model and optional robot model
         self.mapbytes = bytearray(MAP_SIZE_PIXELS * MAP_SIZE_PIXELS)
         self.MIN_SAMPLES = 50
-
-        # Create an iterator to collect scan data from the RPLidar
         self.iterator = self.lidar.iter_scans()
 
     async def update(self):
@@ -36,37 +31,40 @@ class LidarController:
         previous_angles = None
 
         next(self.iterator)
-        while True:  # Keep this infinite for demonstration
+        while True:
 
-            # Extract (quality, angle, distance) triples from current scan
             items = [item for item in next(self.iterator)]
             distances = [item[2] for item in items]
             angles = [item[1] for item in items]
 
-            # todo mack function that gets the speed from the thymio ( left_speed, left_speed)
             left_speed = 0
             right_speed = 0
 
             poses = self.thymio.computePoseChange(time.time(), left_speed, right_speed)
 
-            # Update SLAM with current Lidar scan and scan angles if adequate
             if len(distances) > self.MIN_SAMPLES:
                 self.slam.update(distances, pose_change=poses, scan_angles_degrees=angles)
                 previous_distances = distances.copy()
                 previous_angles = angles.copy()
             elif previous_distances is not None:
                 self.slam.update(previous_distances, scan_angles_degrees=previous_angles)
-            # Get current robot position
+
             self.pose[0], self.pose[1], self.pose[2] = self.slam.getpos()
             print(self.pose[0], self.pose[1], self.pose[2])
 
-            # Get current map bytes as grayscale
             self.slam.getmap(self.mapbytes)
+
+            if self.controller_pipe.poll():
+                message = self.controller_pipe.recv()
+                if message == "FOUND SILVER ORE":
+                    self.handle_silver_ore_detection()
 
             await asyncio.sleep(0.03)
 
-    def publish(self):
+    def handle_silver_ore_detection(self):
+        print("SLAM Algorithm: Detected Silver Ore, Position:", self.pose)
 
+    def publish(self):
         self.viz.publish(self.mapbytes)
         self.viz.publish(
             json.dumps({"x_coord": str(self.pose[0]), "y_coord": str(self.pose[1]), "orientation": str(self.pose[2])}))
@@ -83,9 +81,7 @@ class LidarController:
     def disconnect(self):
         self.lidar.disconnect()
 
-
 Running = True
-
 
 async def mainLoop(lidar, delay):
     task = asyncio.create_task(lidar.update())
@@ -96,12 +92,12 @@ async def mainLoop(lidar, delay):
     lidar.disconnect()
     await task
 
-
 if __name__ == "__main__":
     try:
         thymio = ThymioVehicle(wheel_radius_mm, half_axl_length_mm, None)
         slam = RMHC_SLAM(LaserModel(), MAP_SIZE_PIXELS, MAP_SIZE_METERS)
-        lidar = LidarController(LIDAR_DEVICE, slam, thymio)
+        controller_pipe, slam_pipe = Pipe()
+        lidar = LidarController(LIDAR_DEVICE, slam, thymio, controller_pipe)
         loop = asyncio.run(mainLoop(lidar, 0.5))
     except KeyboardInterrupt:
         Running = False
---
+++ thymio/thymio/RoombaController.py
@@ -0,0 +1,76 @@
+from tdmclient import ClientAsync
+import random
+from multiprocessing import Pipe
+
+class RoombaController:
+    def __init__(self, slam_pipe):
+        self.slam_pipe = slam_pipe
+
+        with ClientAsync() as client:
+
+            async def roomba_behavior():
+                with await client.lock() as node:
+                    await node.wait_for_variables({"prox.horizontal", "ground.ambiant", "leds.top"})
+
+                    initial_ground_color = node.v.ground.ambiant[0]
+
+                    print("Roomba started successfully!")
+
+                    while True:
+                        prox_values = node.v.prox.horizontal
+                        current_ground_color = node.v.ground.ambiant[0]
+
+                        if prox_values[2] > 1500:
+                            if random.random() < 0.5:
+                                self.turn_right(node)
+                            else:
+                                self.turn_left(node)
+                        else:
+                            self.move_forward(node)
+
+                        # Check if silver ore is detected
+                        if self.is_silver_ore_detected(initial_ground_color, current_ground_color):
+                            self.change_behavior(node)
+
+                        await client.sleep(0.3)
+
+                print("Roomba stopped successfully!")
+                self.stop_robot(node)
+
+            client.run_async_program(roomba_behavior)
+
+    def move_forward(self, node):
+        node.v.motor.left.target = 100
+        node.v.motor.right.target = 100
+        node.flush()
+
+    def turn_left(self, node):
+        node.v.motor.left.target = -100
+        node.v.motor.right.target = 100
+        node.flush()
+
+    def turn_right(self, node):
+        node.v.motor.left.target = 100
+        node.v.motor.right.target = -100
+        node.flush()
+
+    def stop_robot(self, node):
+        node.v.motor.left.target = 0
+        node.v.motor.right.target = 0
+        node.flush()
+
+    def is_silver_ore_detected(self, initial_ground_color, current_ground_color):
+        threshold = 100 
+        return abs(current_ground_color - initial_ground_color) > threshold
+
+    def change_behavior(self, node):
+        print("FOUND SILVER ORE, Roomba stopped successfully!")
+        self.slam_pipe.send("FOUND SILVER ORE")
+
+        self.stop_robot(node)
+        node.v.leds.top = [0, 32, 0]
+        node.flush()
+
+if __name__ == "__main__":
+    controller_pipe, slam_pipe = Pipe() 
+    RoombaController(slam_pipe)
