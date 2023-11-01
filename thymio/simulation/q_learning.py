9cf546a67435813f72b81f58d77ad5ad2fda114e
frederikbf1 <frfa@itu.dk>
2023-11-01 10:13:56 +0100

    Update q_learning.py

--- thymio/simulation/q_learning.py
+++ thymio/simulation/q_learning.py
@@ -1,11 +1,11 @@
 import numpy as np
 import random
 
-def next_state(action, actions):
+def next_state(index_of_action):
     """
     Defines the next state based on an action
     """
-    if actions == 0:
+    if index_of_action == 0:
         return 1
     else:
         return -1
@@ -38,20 +38,27 @@ def close_to_wall():
     # Rewards - based on states indices (cm from wall)
     rewards = [
         0, # s1, 100 cm from wall
-        1, # 80 cm from wall
-        2, # 60 cm
+        0, # 80 cm from wall
+        0, # 60 cm
         100, # 40 cm
-        1, # 20 cm
+        0, # 20 cm
         0 # 0 cm from wall
     ]
 
     # Initialize q-table values to 0
     Q = np.zeros((state_size, action_size))
 
+    # All moves
+    moves = []
+    historic_states = []
+
     state = 100 # Initialize state as s1 (the first state)
-    for i in range(5000): # Number of simulation actions
+    for i in range(10): # Number of simulation actions
 
         index_of_state = states.index(state) # Get index of state
+
+        print("CURRENT Q")
+        print(Q)
 
         print("STATE & INDEX OF STATE")
         print(state, index_of_state)
@@ -59,6 +66,7 @@ def close_to_wall():
         ## Takes action based on exploitation/exploration
         epsilon = 0.1 # Percentage of exploration
         if random.uniform(0, 1) < epsilon: # Exploration
+            print("RANDOM CHOICE TAKEN")
             action = random.choice(actions) # Random action in the state
         else: # Exploitation
             index_of_action = Q[index_of_state].argmax() # Get the index of the action at the state with highest reward
@@ -67,14 +75,24 @@ def close_to_wall():
         # From action to index
         index_of_action = actions.index(action)
 
+        # Illegal moves: Continue to next iteration
+        # i.e. backwards on first position, or forward on last position
+        if (index_of_state == 0 and index_of_action == 1) or (index_of_state == state_size-1 and index_of_action == 0):
+            continue
+
+        print("INDEX OF ACTION WITH HIGHEST REWARD")
+        print(index_of_action)
+
         ## Updating Q-values
         lr, gamma = 0.1, 0.9 # Hyperparameters
 
         # Get next state index
-        index_of_next_state = index_of_state + next_state(action, actions)
+        index_of_next_state = index_of_state + next_state(index_of_action)
+        print("INDEX OF NEXT STATE", index_of_next_state)
 
         # Get the reward
         reward = rewards[index_of_next_state]
+        print("REWARD OF NEXT STATE", reward)
 
         #print("> INDICES")
         #print(index_of_state, index_of_action, index_of_next_state)
@@ -82,15 +100,23 @@ def close_to_wall():
         # Updates Q with the future step (action taken)
         Q[index_of_state, index_of_action] = Q[index_of_state, index_of_action] + lr * (reward + gamma * np.max(Q[index_of_next_state, :]) - Q[index_of_state, index_of_action])
 
+        # History of states and moves
+        historic_states.append(state)
+        moves.append(action)
+
         # Updates state before next iteration
         state = states[index_of_next_state]
 
         print("Action", action)
 
-        print(Q)
+        #print(Q)
 
         print("---")
         print("\n")
+
+    print(moves)
+    print(historic_states)
+    print(Q)
 
 
 close_to_wall()
